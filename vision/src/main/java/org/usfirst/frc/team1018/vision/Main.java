9c8bbab01a18c4fede1a8d969a8eecaad6c72aec
firecrafty <bluer2288@students.pike.k12.in.us>
2017-09-07 12:21:23 -0400

    Vision codebase

---
+++ vision/src/main/java/org/usfirst/frc/team1018/vision/Main.java
@@ -0,0 +1,85 @@
+package org.usfirst.frc.team1018.vision;
+
+import edu.wpi.cscore.*;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
+
+public class Main {
+    public static void main(String[] args) {
+        // Loads our OpenCV library. This MUST be included
+        System.loadLibrary("opencv_java310");
+
+        // Connect NetworkTables, and get access to the publishing table
+        NetworkTable.setClientMode();
+        // Set your team number here
+        NetworkTable.setTeam(1018);
+
+        NetworkTable.initialize();
+
+
+        // This is the network port you want to stream the raw received image to
+        // By rules, this has to be between 1180 and 1190, so 1185 is a good choice
+        int streamPort = 1185;
+
+        // This gets the image from a USB camera
+        // Usually this will be on device 0, but there are other overloads
+        // that can be used
+        UsbCamera camera = new UsbCamera("CoprocessorCamera", 0);
+        // Set the resolution for our camera, since this is over USB
+        camera.setResolution(640, 480);
+        // This stores our reference to our mjpeg server for streaming the input image
+        MjpegServer inputStream = new MjpegServer("MJPEG Server", streamPort);
+        inputStream.setSource(camera);
+
+        // This creates a CvSink for us to use. This grabs images from our selected camera,
+        // and will allow us to use those images in opencv
+        CvSink imageSink = new CvSink("CV Image Grabber");
+        imageSink.setSource(camera);
+
+        // This creates a CvSource to use. This will take in a Mat image that has had OpenCV operations
+        // operations
+        CvSource imageSource = new CvSource("CV Image Source", VideoMode.PixelFormat.kMJPEG, 640, 480, 30);
+        MjpegServer cvStream = new MjpegServer("CV Image Stream", 1186);
+        cvStream.setSource(imageSource);
+
+        // All Mats and Lists should be stored outside the loop to avoid allocations
+        // as they are expensive to create
+        Mat inputImage = new Mat();
+        Mat hsv = new Mat();
+
+        TargetPipeline pipeline = new TargetPipeline();
+        // Infinitely process image
+        while(true) {
+            // Grab a frame. If it has a frame time of 0, there was an error.
+            // Just skip and continue
+            long frameTime = imageSink.grabFrame(inputImage);
+            if(frameTime == 0) continue;
+
+            // Below is where you would do your OpenCV operations on the provided image
+            // The sample below just changes color source to HSV
+            pipeline.process(inputImage);
+            if(!pipeline.findContoursOutput().isEmpty()) {
+                Rect r = Imgproc.boundingRect(pipeline.findContoursOutput().get(0));
+
+
+            }
+
+            // Here is where you would write a processed image that you want to restreams
+            // This will most likely be a marked up image of what the camera sees
+            // For now, we are just going to stream the HSV image
+            imageSource.putFrame(hsv);
+        }
+    }
+
+    private static UsbCamera setUsbCamera(int cameraId, MjpegServer server) {
+        // This gets the image from a USB camera
+        // Usually this will be on device 0, but there are other overloads
+        // that can be used
+
+        UsbCamera camera = new UsbCamera("CoprocessorCamera", cameraId);
+        server.setSource(camera);
+        return camera;
+    }
+}
